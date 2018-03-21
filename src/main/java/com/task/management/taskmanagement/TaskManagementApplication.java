902b313074da29a6738f51bd5f15035d37ceb106
mohanrajselvam <mohanraj.s@consulting.active.ai>
2018-03-21 21:29:18 +0530

    Task-Management
    #Initial project setup.

---
+++ src/main/java/com/task/management/taskmanagement/TaskManagementApplication.java
@@ -0,0 +1,20 @@
+package com.task.management.taskmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+ * <p>
+ * TaskManagementApplication Spring-Boot Application class.
+ * </p>
+ *
+ * @author Mohanraj Created on March 21, 2018.
+ */
+@SpringBootApplication
+public class TaskManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskManagementApplication.class, args);
+	}
+}
---
+++ src/test/java/com/task/management/taskmanagement/TaskManagementApplicationTests.java
@@ -0,0 +1,23 @@
+package com.task.management.taskmanagement;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * <p>
+ * TaskManagementApplicationTests Spring-Boot Test Application class.
+ * </p>
+ *
+ * @author Mohanraj Created on March 21, 2018.
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TaskManagementApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
